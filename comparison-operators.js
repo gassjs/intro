6dc71dfef98501ec719603b76617f8474bf98a7b
gassjs <bondi7@tpg.com.au>
2017-10-16 19:51:23 +1100

    Add Lesson 3

---
+++ comparison-operators.js
@@ -0,0 +1,21 @@
+console.log(1 === 1)
+
+console.log(15 > 10)
+
+console.log('cat' === 'cat')
+
+console.log(1 + 2 == 4 - 1)
+
+
+// Change the _comparison_ operators of the following to log true:
+//
+console.log(1 >= 0)
+
+console.log('77' !== 77)
+
+console.log(20 === 20)
+
+console.log('happy' != 'sad')
+
+// // explain the last console.log()
+//
---
+++ logical-operators.js
@@ -0,0 +1,32 @@
+console.log(true && true || false)
+
+console.log(((2 + 2) === 4) && 4 - 1 === 3 || "quick maths")
+
+console.log("" && false)
+
+console.log(false && false)
+
+const country = 'australia'
+const age = 21
+console.log(age >= 21 && country != 'usa')
+
+console.log(!false)
+
+console.log(!(false) && !!(false) && !!!(false) && !!!!(false))
+
+// Change the a value in the following to log 'quick maths'
+//
+// console.log(((2 + '2') === 4) && 4 - 1 === 3 && 'quick maths')
+//
+// // Change the _logical_ operators of the following to log true:
+//
+// const access = 'admin'
+// const name = 'jess'
+// console.log(access === 'admin' && name === 'nick')
+//
+// console.log(1 + 2 - 3 >= 0 && 5 + 5 === 10 && "cat" === "dog")
+//
+// console.log(!true)
+//
+// console.log(!!!true || !!!true)
+//
