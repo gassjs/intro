9b48e90574ce9e7c6a718903e237530a67ae18a3
gassjs <bondi7@tpg.com.au>
2017-10-18 12:25:39 +1100

    Add data-types

---
+++ 02-data-types/comparison-operators.js
@@ -0,0 +1,26 @@
+// comparison-operators.js
+// Fix each statement below to return true by either changing
+// values or operators.
+
+// Change the values of the following to log true:
+
+console.log('1' === 1)
+
+console.log(15 > 10)
+
+console.log('cat' === 'dog')
+
+console.log(1 + 2 > 4 - 1)
+
+
+// Change the _comparison_ operators of the following to log true:
+
+console.log(1 <= 0)
+
+console.log('77' !== 77)
+
+console.log(20 > 20)
+
+console.log('happy' > 'sad')
+
+// explain the last console.log()
